5267699170ba4820c0b06f3a0dce39f9b6645e8d
Basyrov Rustam <hrustbas@gmail.com>
2025-05-05 04:11:23 +0300

    freq_table

--- main.c
+++ main.c
@@ -10,6 +10,8 @@ do {									\
 #define FILEPATH "data/data.fa"
 #define FILEPATH2 "data/data2.fa"
 
+size_t verbose = 0;
+
 typedef unsigned char u8;
 
 typedef struct {
@@ -17,10 +19,23 @@ typedef struct {
   size_t size;
 } raw_data;
 
-size_t verbose = 0;
+typedef struct {
+  u8 symbol;
+  size_t count;
+} freq;
+
+#define FREQ_TABLE_SIZE 256
+typedef freq freq_table[FREQ_TABLE_SIZE];
 
 int read_file(const char* filepath, raw_data *rd, int verbose);
 int write_to_file(const char* filepath, raw_data *rd);
+
+size_t get_index_from_ft(freq_table *ft, u8 symbol) {
+  for (size_t i=0; i<FREQ_TABLE_SIZE; i++)
+    if (ft[i]->symbol == symbol)
+      return i;
+  exit(6969);
+}
 
 int count_freqs(unsigned long *ft, raw_data *rd) {
   if (verbose)
